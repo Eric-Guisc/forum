867b124e28daa99b5bf18bbaf231b1d46543424a
Eric-Guisc <1091877410@qq.com>
2021-07-06 15:43:28 +0800

    添加评论

--- src/main/java/priv/gsc/forum/service/CommentService.java
+++ src/main/java/priv/gsc/forum/service/CommentService.java
@@ -2,16 +2,26 @@ package priv.gsc.forum.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 import priv.gsc.forum.dao.CommentMapper;
+import priv.gsc.forum.dao.DiscussPostMapper;
 import priv.gsc.forum.entity.Comment;
+import priv.gsc.forum.util.ForumConstant;
+import priv.gsc.forum.util.SensitiveFilter;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements ForumConstant {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType,entityId,offset,limit);
@@ -19,6 +29,25 @@ public class CommentService {
 
     public int findCommentCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    public int addComment(Comment comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+
+        // 先添加评论
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // 再更新帖子评论数量（该条评论是帖子的评论，而不是回复；如果该评论是条回复，则不更新帖子中的评论数量，在DiscussPostController中会进行查询回复的数量）
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostMapper.updateCommentCount(comment.getEntityId(), count);
+        }
+
+        return rows;
     }
 
 }
