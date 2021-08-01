fd26bb9c045393add52fd29e99efd89dff9dfbd1
Eric-Guisc <1091877410@qq.com>
2021-08-01 22:31:36 +0800

    Spring整合Redis

---
+++ src/test/java/priv/gsc/forum/test/RedisTest.java
@@ -0,0 +1,56 @@
+package priv.gsc.forum.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import priv.gsc.forum.ForumApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = ForumApplication.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings() {
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    // 编程式事务
+    @Test
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+
+                // 开启事务
+                operations.multi();
+
+                operations.opsForSet().add(redisKey, "zhangsan");
+                operations.opsForSet().add(redisKey, "lisi");
+                operations.opsForSet().add(redisKey, "wangwu");
+
+                System.out.println(operations.opsForSet().members(redisKey));   // 在事务中做查询，此时数据还没有在事务中提交，所以为空
+
+                return operations.exec();   // 提交事务
+            }
+        });
+
+        System.out.println(obj);
+        System.out.println(redisTemplate.opsForSet().members("test:tx"));
+    }
+}
